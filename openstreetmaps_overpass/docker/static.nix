5838d243e05788e340e877f83925a255c2a9c687
Daniel Perry <dperry@danieltperry.me>
2022-10-21 22:53:53 -0400

    Image builds successfully and runs in docker.

--- openstreetmaps_overpass/docker/static.nix
+++ openstreetmaps_overpass/docker/static.nix
@@ -1,15 +1,45 @@
 # Static osm-3s + httpd docker image
-# Contains Apache configuration for hosting an OSM database.
-# Database is expected to exist under <TODO>
+# Uses httpd docker image as a base and creates a layered image with osm-3s binaries.
+# Does not perform any updates on the attached OSM database.
+#
+# Expected mount points:
+#   - /mnt/osm (Data directory for OSM, configured with `osmDataDir`)
+#     - Database is expected to exist under ./db/ (Can be configured with `osmRelativeDbDir`)
+#     - Diffs (if used) are expected to exist under ./diffs/
+#   - /mnt/log (Logging directory, configured with `logDir`)
+#
+# TODO: Contains Apache configuration for hosting an OSM database.
+#
 # No updates are automatically applied to the database.
-# Docker image contains only osm-3s + dependencies with httpd as a base.
-# To run, `<TODO>`
+#   - fetch_osm.sh is not started.
+#   - apply_osc_to_db.sh is not started.
+#
+# To run:
+# ```
+# docker run osm-3s-static:latest       \
+# -v <host-osm-data-location>:/mnt/osm  \
+# -v <host-log-location>:/mnt/log
+# ```
+#
+# To create and populate a new OSM database:
+# ```
+# docker run osm-3s-static:latest       \
+# -v <host-osm-data-location>:/mnt/osm  \
+# -v <host-log-location>:/mnt/log       \
+# /bin/download_clone.sh --db-dir=/mnt/osm/db --source=http://dev.overpass-api.de/api_drolbr/ --meta=no
+# ```
 
 { pkgs ? import <nixpkgs> {}
 }:
 
 let
-  osm = import ../osm-3s.nix {};
+  osm3s = import ../osm-3s.nix {};
+  # OSM Settings
+  osmDataDir = "/mnt/osm"; # Where in the docker image the root OSM directory is located.
+  osmRelativeDbDir = "db"; # Where, relative to osmDataDir, the db directory is located.
+  logDir = "/mnt/log"; # Where in the docker image logs should be written to.
+  
+  # Apache/httpd docker image configuration
   httpdPlatformImages = {
     "x86_64" = {
       imageDigest = "sha256:15515209fb17e06010fa5af6fe15fa0351805cc12acfe82771c7724f06c34ae4";
@@ -28,7 +58,7 @@ pkgs.dockerTools.buildLayeredImage {
   name = "osm-3s-static";
   tag = "latest";
   contents = [
-    osm
+    osm3s
   ];
   fromImage = pkgs.dockerTools.pullImage {
     imageName = "httpd";
@@ -36,5 +66,16 @@ pkgs.dockerTools.buildLayeredImage {
     sha256 = currentHttpdPlatformImage.sha256;
     finalImageTag = httpdImageTag;
     finalImageName = httpdImageName;
+  };
+  extraCommands = ''
+  # Create launch script
+  # Launch osm dispatcher daemon (not necessary for static host)
+  echo "${osm3s}/bin/dispatcher --osm-base --db-dir=${osmDataDir}/${osmRelativeDbDir} 1>${logDir}/dispatcher.log 2>&1 &" >> ./start_server.sh
+
+  # Launch apache/httpd
+  echo "/usr/local/bin/httpd-foreground" >> ./start_server.sh
+'';
+  config = {
+    Cmd = ["${pkgs.bash}/bin/bash" "-c" "./start_server.sh"];
   };
 }
